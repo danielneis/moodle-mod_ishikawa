8cfc38edd32ff3138e6c562ca07a72d869e85947
caiodoneda <caiodoneda@gmail.com>
2012-12-05 14:55:48 -0200

    construindo a arvore para o backup

--- backup/moodle2/backup_ishikawa_stepslib.php
+++ backup/moodle2/backup_ishikawa_stepslib.php
@@ -13,36 +13,54 @@ class backup_choice_activity_structure_step extends backup_activity_structure_st
           $userinfo = $this->get_setting_value('userinfo');
           
           // Define each element separated
-          $choice = new backup_nested_element('ishikawa', array('id'), array(
+          $ishikawa = new backup_nested_element('ishikawa', array('id'), array(
                                               'name', 'intro', 'maxchar', 'maxlines',
                                               'maxcolumns', 'grade', 'preventlate', 'timedue',
                                               'timeavaliable', 'introformat', 'timemodified'));
           
-          $options = new backup_nested_element('ishikawa_axis_blocks');
-          $option = new backup_nested_element('ishikawa_axis_block', array('id'), array(
+          $ishikawa_axis_blocks = new backup_nested_element('ishikawa_axis_blocks');
+          $ishikawa_axis_block = new backup_nested_element('ishikawa_axis_block', array('id'), array(
                                               'submissionid', 'nivel_x', 'texto', 'cor'));
                             
-          $answers = new backup_nested_element('ishikawa_causes_blocks');
-          $answer = new backup_nested_element('ishikawa_causes_block', array('id'), array(
+          $ishikawa_causes_blocks = new backup_nested_element('ishikawa_causes_blocks');
+          $ishikawa_causes_block = new backup_nested_element('ishikawa_causes_block', array('id'), array(
                                               'submissionid', 'nivel_x', 'nivel_y', 'texto' ,'cor'));
 
-          $answers = new backup_nested_element('ishikawa_connections');
-          $answer = new backup_nested_element('ishikawa_connection', array('id'), array(
+          $ishikawa_connections = new backup_nested_element('ishikawa_connections');
+          $ishikwa_connection = new backup_nested_element('ishikawa_connection', array('id'), array(
                                               'submissionid', 'src_id', 'src_type', 'dst_id' ,'dst_type'));
           
-          $answers = new backup_nested_element('ishikawa_consequences_blocks');
-          $answer = new backup_nested_element('ishikawa_consequences_block', array('id'), array(
+          $ishikawa_consequences_blocks = new backup_nested_element('ishikawa_consequences_blocks');
+          $ishikawa_consequences_block = new backup_nested_element('ishikawa_consequences_block', array('id'), array(
                                               'submissionid', 'nivel_x', 'nivel_y', 'texto' ,'cor'));
           
-          $answers = new backup_nested_element('ishikawa_grades');
-          $answer = new backup_nested_element('ishikawa_grade', array('id'), array(
+          $ishikawa_grades = new backup_nested_element('ishikawa_grades');
+          $ishikawa_grade = new backup_nested_element('ishikawa_grade', array('id'), array(
                                               'userid', 'grade', 'feedback', 'timecreated' ,'timemodified'));
            
-          $answers = new backup_nested_element('ishikawa_submissions');
-          $answer = new backup_nested_element('ishikawa_submission', array('id'), array(
+          $ishikawa_submissions = new backup_nested_element('ishikawa_submissions');
+          $ishikawa_submission = new backup_nested_element('ishikawa_submission', array('id'), array(
                                               'ishikawaid', 'userid', 'tail_text', 'head_text' ,'timecreated', 'timemodified'));
           
           // Build the tree
+          $ishikawa->add_child($ishikawa_axis_blocks);
+          $ishikawa_axis_blocks->add_child($ishikawa_axis_block);
+
+          $ishikawa->add_child($ishikawa_causes_blocks);
+          $ishikawa_causes_blocks->add_child($ishikawa_causes_block);
+          
+          $ishikawa->add_child($ishikawa_connections);
+          $ishikawa_connections->add_child($ishikawa_connection);
+
+          $ishikawa->add_child($ishikawa_consequences_blocks);
+          $ishikawa_consequences_blocks->add_child($ishikawa_consequences_block);          
+          
+          $ishikawa->add_child($ishikawa_grades);
+          $ishikawa_grades->add_child($ishikawa_grade);          
+
+          $ishikawa->add_child($ishikawa_submissions);
+          $ishikawa_submissions->add_child($ishikawa_submission);
+          
           
           // Define sources
           
@@ -51,5 +69,6 @@ class backup_choice_activity_structure_step extends backup_activity_structure_st
           // Define file annotations
           
           // Return the root element (choice), wrapped into standard activity structure
-          }
+          return $this->prepare_activity_structure($ishikawa); 
+         }
   }
